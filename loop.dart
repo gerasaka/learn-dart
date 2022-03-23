1d7f804e4a2de25345675a413ceb344aeb097f5c
gerasaka <gerryjulioo@gmail.com>
2022-03-23 15:40:00 +0700

    basic loop syntax in dart

---
+++ loop.dart
@@ -0,0 +1,19 @@
+void main() {
+  List<String> menu = ['Nasi Goreng', 'Rendang', 'Indomie'];
+
+  // int i = 0;
+
+  // while(i < menu.length) {
+  //   print(menu[i]);
+  //   i++;
+  // }
+
+  // do {
+  //   print(menu[i]);
+  //   i++;
+  // } while(i < menu.length);
+
+  for(int i = 0; i < menu.length; i++) {
+    print(menu[i]);
+  }
+}
