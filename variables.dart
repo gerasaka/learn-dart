7f2072b727b5b2f122bd1a6af58ef8606c7940ac
gerasaka <gerryjulioo@gmail.com>
2022-03-23 07:50:32 +0700

    variables declaration

---
+++ variables.dart
@@ -0,0 +1,13 @@
+void main() {
+  var name = 'my name'; //dinamyc data type, the value could be string, number, map(object), or list
+  String text;
+  int number;
+  double decimal;
+  List myArray = [1,2,3]; //dynamic list (default: List<dynamic>), others: List<int>, List<String>, etc.
+  Map myObject = {'key': 1, 'other': 1}; //dynamic Map (default: Map(dynamic, dynamic))
+  bool iAmCool = true;
+
+  print('hello');
+  print('this is ${myObject}'); //template literals
+  print(myObject['key']); //access map value
+}
